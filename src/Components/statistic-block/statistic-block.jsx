cfe80e30a1fbbfc76ddb469ca3ea9aa0417f6a78
st0pl <st0pl0gin@protonmail.com>
2026-06-18 14:24:06 +0700

    Удаление неиспользуемого кода. Правки .gitignore

--- src/Components/statistic-block/statistic-block.jsx
+++ src/Components/statistic-block/statistic-block.jsx
@@ -48,18 +48,3 @@ function StatisticBlock({ stats }) {
 }
 
 export default memo(StatisticBlock)
-
-function StatisticBlock2( { stats } ) {
-
-    return (
-        <Panel title='Статистика' color='#bb86fc'>
-            <p>Молодые деревья: <b style={{ color: COLORS[STATES.YOUNG] }}>{stats.young}</b></p>
-            <p>Зрелые деревья: <b style={{ color: COLORS[STATES.ADULT] }}>{stats.adult}</b></p>
-            <p>Старые деревья: <b style={{ color: COLORS[STATES.OLD] }}>{stats.old}</b></p>
-            <p>Сухостои: <b style={{ color: COLORS[STATES.DEAD] }}>{stats.dead}</b></p>
-            <p>Очаги возгораний: <b style={{ color: COLORS[STATES.FIRE] }}>{stats.fire}</b></p>
-            <p>Выгоревшие участки: <b style={{ color: COLORS[STATES.ASH] }}>{stats.ash}</b></p>
-         
-        </Panel>
-    );
-}
